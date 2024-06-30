56fdf390a88886b5b5e393428e3e3d50a9f119a7
gitThinh <111752628+gitThinh@users.noreply.github.com>
2024-07-01 00:16:27 +0700

    fix deploy

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
-
 import { languages, fallbackLng } from "@/app/i18n/config-lang";
-import DropDownlanguage from "@/components/DropDownLanguage";
-import Navbar from "./(Header)/Navbar";
 import Header from "./(Header)/Header";
 
-export default function Home({
+const Home = ({
   params: { lang },
 }: {
   params: {
     lang: string;
   };
-}) {
+}) => {
   if (languages.indexOf(lang) < 0) lang = fallbackLng;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black dark:text-white">
@@ -20,4 +16,6 @@ export default function Home({
       </div>
     </main>
   );
-}
+};
+
+export default Home;
